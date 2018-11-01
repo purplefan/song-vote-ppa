1c0aa42cc9618dc374f2850ba2ca80fc030034aa
Grzegorz Olejarz <g.olejarz@gmail.com>
2018-11-01 12:40:08 +0100

    Domain Song repository implementation added

---
+++ src/Repository/SongRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repository;
+
+use App\Domain\Entity\Song;
+use App\Domain\Repository\SongRepositoryInterface;
+use App\External\ExternalSongRepository;
+
+class SongRepository implements SongRepositoryInterface
+{
+
+    public function findSong(int $id): Song
+    {
+        return new Song(ExternalSongRepository::getSongById($id));
+    }
+}
