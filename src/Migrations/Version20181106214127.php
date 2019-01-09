7c4449a0c0d7ab8ddaae20fb0f80f8420e56315f
Grzegorz Olejarz <g.olejarz@gmail.com>
2019-01-09 13:50:33 +0100

    fixed column name

--- src/Migrations/Version20181106214127.php
+++ src/Migrations/Version20181106214127.php
@@ -15,7 +15,7 @@ final class Version20181106214127 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('CREATE TABLE song (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, song_details VARCHAR(255) DEFAULT NULL, user_email DOUBLE PRECISION DEFAULT NULL)');
+        $this->addSql('CREATE TABLE song (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, song_details VARCHAR(255) DEFAULT NULL, score DOUBLE PRECISION DEFAULT NULL)');
     }
 
     public function down(Schema $schema) : void
